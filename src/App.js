9e62bea1fbeabf229385d71f5439137d5c7eac7b
AGFarhat <assad.george.farhat@gmail.com>
2017-10-13 19:13:23 +0300

    fixed merge n2

--- src/App.js
+++ src/App.js
@@ -38,28 +38,17 @@ export default class App extends Component<{}> {
 
   render() {
     const { isRaining, position, weather, lastUpdated, remark } = this.state;
-<<<<<<< HEAD
     if (!weather || !position) return <View style={ styles.spinnerContainer }><Spinner/></View>;
-=======
-    if (!weather || !position) return <View style={ styles.container }><Spinner/></View>;
->>>>>>> d15cdae0dc83c2db51b04d7e9339a14dd6cea1ed
 
     return (
       <View style = { styles.container }>
         <View style = {{ height: 100 }}/>
 
         <View style = { styles.tempContainer }>
-<<<<<<< HEAD
           <Text style={ styles.textStyle }>{ Math.round(weather.main.temp)+"\u2103" }</Text>
           <Text></Text>
           <Text>Bring an umbrella? { isRaining ? 'Probably...' : 'Nope' }</Text>
           <Text>{ weather.weather[0].main }</Text>
-=======
-          <Text style={ styles.textStyle }>{ Math.round((weather.main.temp-32)/1.8)+"\u2103" }</Text>
-          <Text></Text>
-          <Text>Bring an umbrella? { isRaining ? 'Probably...' : 'Nope' }</Text>
-          <Text>{ weather.weather[0].description }</Text>
->>>>>>> d15cdae0dc83c2db51b04d7e9339a14dd6cea1ed
         </View>
 
         <View style = { styles.tempContainer }>
@@ -84,14 +73,9 @@ const styles = {
     fontSize:50
   },
   spinnerContainer : {
-<<<<<<< HEAD
     flex:1,
     padding:5,
     justifyContent: 'center',
-=======
-    padding:5,
-    justifyContent: 'flex-start',
->>>>>>> d15cdae0dc83c2db51b04d7e9339a14dd6cea1ed
     position:'relative'
   },
   tempContainer : {

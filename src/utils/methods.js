a308115ddeaa245ad41bc0b7ba8e9c55096f2f71
AGFarhat <assad.george.farhat@gmail.com>
2017-11-12 12:28:15 +0000

    almost there

--- src/utils/methods.js
+++ src/utils/methods.js
@@ -4,35 +4,8 @@ import { KEY } from './constants';
 
 const REFRESH_TIME = 60000; // time required before second refresh (ms)
 
-// Uncomment this if you want to clearstorage. dont fetch api too often. clear once.
-// AsyncStorage.clear();
-
-//const sampleWeather = {"coord":{"lon":139.01,"lat":35.02},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":285.514,"pressure":1013.75,"humidity":100,"temp_min":285.514,"temp_max":285.514,"sea_level":1023.22,"grnd_level":1013.75},"wind":{"speed":5.52,"deg":311},"clouds":{"all":0},"dt":1485792967,"sys":{"message":0.0025,"country":"JP","sunrise":1485726240,"sunset":1485763863},"id":1907296,"name":"Tawarano","cod":200};
-
 const utils = {
   isAndroid: () => Platform.OS === 'android',
-
-  fetchSettings: async () => {
-    const localSettings = await utils.getLocalData(KEY.SETTINGS);
-    if (localSettings === null) {
-      const initDate = new Date();
-      initDate.setHours('07');
-      initDate.setMinutes('00');
-      const sevenAmDate = initDate.toDateString();
-      const storedState = {
-        date: sevenAmDate,
-        isNotifyOn: false,
-      };
-      await utils.setLocalData(KEY.SETTINGS, storedState);
-      return storedState;
-    }
-
-    return JSON.parse(localSettings);
-  },
-
-  setSettings: async (stateData) => {
-    await utils.setLocalData(KEY.SETTINGS, stateData);
-  },
 
   getCurrentPosition: () => {
     return new Promise((resolve) => {
@@ -41,8 +14,9 @@ const utils = {
   },
 
   getCurrentWeather: async ({latitude,longitude}) => {
-    const URL_BASE = 'https://api.openweathermap.org/data/2.5/forecast?';
-    // const url = 'https://samples.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1';
+    // const URL_BASE = 'http://api.openweathermap.org/data/2.5/weather?';
+    const URL_BASE = 'http://api.openweathermap.org/data/2.5/forecast?';
+    //const url = 'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1';
     const url = `${URL_BASE}lat=${latitude}&lon=${longitude}&appid=${API}&units=metric`;
     return new Promise((resolve) => {
       fetch(url).then(response => {
@@ -50,12 +24,13 @@ const utils = {
         if (bodyText && typeof response._bodyText === 'string') {
           resolve(JSON.parse(response._bodyText));
         } else {
-          resolve(response.data);          
+          resolve(response.data);
         }
       }).catch((error) => {
         throw new Error(error)
       });
     });
+    //return(null);
   },
 
   retrieveDayForecast: async (data,count) => {
@@ -88,7 +63,9 @@ const utils = {
       // check each item, then refetch if needed
       position = await utils.getCurrentPosition();
       weather = await utils.getCurrentWeather(position.coords);
+      console.log(weather);
       let { description, isRaining } = await utils.retrieveDayForecast(weather,weather.cnt);
+      console.log(isRaining);
       lastUpdated = await utils.getCurrentTime();
 
       await utils.setLocalData(KEY.WEATHER, { position, weather, lastUpdated, description, isRaining });
@@ -101,7 +78,6 @@ const utils = {
   getCachedItems: async () => {
     // Get the localdata
     const localStore = await utils.getLocalData(KEY.WEATHER);
-    console.log(localStore);
     if (localStore === null) {
       const position = await utils.getCurrentPosition(); // TODO catch
       const weatherData = {
@@ -115,6 +91,23 @@ const utils = {
       return weatherData;
     }
     return JSON.parse(localStore);
+  },
+
+  fetchSettings: async () => {
+    const localSettings = await utils.getLocalData(KEY.SETTINGS);
+    if (localSettings === null) {
+      const initDate = new Date();
+      initDate.setHours('07');
+      initDate.setMinutes('00');
+      const sevenAmDate = initDate.toDateString();
+      const storedState = {
+        date: sevenAmDate,
+        isNotifyOn: false,
+      };
+      await utils.setLocalData(KEY.SETTINGS, storedState);
+      return storedState;
+    }
+    return JSON.parse(localSettings);
   },
 
   setCachedSettings: async (settingData) => {

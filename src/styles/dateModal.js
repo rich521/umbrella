7ca02669d22570789acdf42150748ce2757b62e3
Richard Strutt <richardastrutt@gmail.com>
2017-11-20 18:24:08 +0000

    styles yea (#25)

---
+++ src/styles/dateModal.js
@@ -0,0 +1,24 @@
+import { fontPrimaryColor } from './app';
+
+const styles = {
+  container: {
+    marginTop: 50,
+  },
+  title: {
+    color: fontPrimaryColor,
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 50,
+  },
+  time: {
+    textAlign: 'right',
+    color: fontPrimaryColor,
+    fontSize: 20,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'stretch',
+  }
+};
+
+export default styles;
